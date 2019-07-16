f74bdd75e5b8119231d7046af5835b1995c445a3
Damian Dailisan <damian.dailisan@gmail.com>
2019-07-16 14:06:46 +0800

    parameters as function arguments

--- periodic_stations.py
+++ periodic_stations.py
@@ -6,22 +6,28 @@ import itertools
 import csv
 
 sim_time = 3000
-trans_time = 1000
+trans_time = 3000
 num_lanes = 1
-roadlength = 500
+roadlength = 5000
+p_slow = 0.1
+v_max = 5
 
 densities = np.arange(0.02, 1, 0.02)
-bus_fractions = np.linspace(0, 1/num_lanes, 5)
+# bus_fractions = np.linspace(0, 1/num_lanes, 5)
+bus_fractions = np.arange(0, 0.101, 0.01)
 trials = range(50)
-station_periods = (roadlength*(0.5)**np.arange(np.log2(roadlength))).astype(int)
-alphas = station_periods/roadlength*2
+alpha_max = 0.1
+station_periods = (roadlength*(0.5)**np.arange(1, np.log2(roadlength))).astype(int)
+alphas = station_periods/roadlength*(2*alpha_max)
+print(station_periods)
 
 def g(tup):
-    return simulate(*tup, num_lanes=num_lanes, sim_time=sim_time, trans_time=trans_time)
+    return simulate(*tup, station_period=tup[-1]*roadlength/(2*alpha_max), num_lanes=num_lanes, 
+                    sim_time=sim_time, trans_time=trans_time, roadlength=roadlength, v_max=v_max, p_slow=p_slow)
 p = Pool()
 
 
-with open('periodic_stations.csv', 'w') as f:
+with open('periodic_stations_maxalpha_%s_bf.csv'%alpha_max, 'w') as f:
     writer = csv.writer(f)
     lines = 0
     for result in p.imap_unordered(g, itertools.product(densities, bus_fractions, trials, alphas)):
